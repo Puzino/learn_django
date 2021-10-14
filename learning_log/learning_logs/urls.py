b185e956ab986851b04a4a1e58642632215fec6a
Timur <puzino000@gmail.com>
2021-10-14 12:42:05 +0300

    Глава 18
    Добавил страницы: домашняя, темы, сортировка по датам, переход от базовой страницы

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -5,5 +5,9 @@ from . import views
 app_name = 'learning_logs'
 urlpatterns = [
     # домашняя страница
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    # страница со списком тем
+    path('topics/', views.topics, name='topics'),
+    # страница с подробной информацией по отдельной теме
+    path('topics/<int:topic_id>/', views.topic, name='topic')
 ]
